5c844ca97bc9c47bd04a5e9d1c08a2fdfd2eb1c9
Jorge Calero <jorge.calero@gmail.com>
2023-02-16 10:38:53 +0100

    Maven MVC

---
+++ MavenMVC/src/main/java/com/jcalero/MavenMVC/config/App.java
@@ -0,0 +1,24 @@
+package com.jcalero.MavenMVC.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.jcalero.MavenMVC")
+public class App 
+{
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+    
+}
